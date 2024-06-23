f5202c20d401089f1b646e9c39ed87817951c698
Thejus Rajendran <thejusr1999@icloud.com>
2024-06-23 23:21:10 +0530

    - added content to the mainpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
 export default function HomePage() {
-  return <div className="">HomePage</div>;
+  return (
+    <div className="flex">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-2">
+          <h5 className="section-title">Litte bit about myself</h5>
+          <p className="text-lg font-normal max-w-lg">
+            a product designer working in bengaluru, who specialises on
+            interface & experience design
+          </p>
+        </div>
+        <hr />
+        <div className="flex flex-col gap-2">
+          <h5 className="section-title">Clients that i have worked with</h5>
+          <p className="text-lg font-normal max-w-lg">
+            a product designer working in bengaluru, who specialises on
+            interface & experience design
+          </p>
+        </div>
+      </div>
+      <div>col-2</div>
+    </div>
+  );
 }
--- src/components/SideNav/NavLink.tsx
+++ src/components/SideNav/NavLink.tsx
@@ -11,8 +11,8 @@ export default function NavLink(props: NavLinkProps) {
     <div
       className={`${
         props.isActive
-          ? "bg-gray-900 text-white"
-          : "bg-white text-gray-900 outline outline-1 outline-gray-200"
+          ? "bg-gray-900 hover:bg-gray-800 text-white"
+          : "bg-white hover:bg-gray-100 text-gray-900 outline outline-1 outline-gray-200"
       } h-12 rounded-2xl flex gap-2 transition-colors duration-300 items-center p-4`}
     >
       <Image
